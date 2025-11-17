8e05ae3c94d68a4f2e533d3d0e60ad8c646a776a
name used in GitHub <your mail id used in GitHub>
2025-11-17 22:49:48 +0530

    fix settings

--- shan_auditors/settings.py
+++ shan_auditors/settings.py
@@ -159,7 +159,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # replace with your company email
+EMAIL_HOST_USER = 'shanassociatestnj@gmail.com'  # replace with your company email
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')      # use app password, not real Gmail password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
