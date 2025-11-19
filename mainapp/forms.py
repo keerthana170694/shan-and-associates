3e4efc43d5d09f5517beb3dad951e5fee03a902e
name used in GitHub <your mail id used in GitHub>
2025-11-19 14:50:02 +0530

    add all changes

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Contact
+from django.core.validators import RegexValidator
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -11,5 +12,18 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label='Your Name')
     email = forms.EmailField(label='Your Email')
-    phone = forms.CharField(max_length=15, label='Phone Number', required=False)
+    phone = forms.CharField(
+        max_length=15,
+        validators=[
+            RegexValidator(
+                regex=r'^[0-9]+$',
+                message='Phone number must contain digits only.'
+            )
+            ],
+            widget=forms.TextInput(attrs={
+                'placeholder': 'Enter phone number',
+                'pattern': '[0-9]+',
+                'title': 'Only numbers allowed'
+            })
+    )
     message = forms.CharField(widget=forms.Textarea, label='Message')
